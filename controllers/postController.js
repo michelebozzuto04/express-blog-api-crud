80c0e18802db8e1e718a44199fdcaafbddacd1c6
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-07-01 15:45:46 +0200

    refactor: clean up comments in show and destroy functions for better readability

--- controllers/postController.js
+++ controllers/postController.js
@@ -15,17 +15,17 @@ function show(req, res) {
     const id = parseInt(req.params.id);
 
     const post = posts.find(post => post.id === id);
-    // Facciamo il controllo
+
     if (!post) {
-        //Imposto lo status 404
+
         res.status(404)
-        // Restituisco un JSON con le altre informazioni
+
         return res.json({
             error: "Not Found",
             message: "Post non trovato"
         })
     }
-    // Restituiamolo sotto forma di JSON
+
     res.json(post);
 }
 
@@ -49,6 +49,7 @@ function destroy(req, res) {
     const post = posts.find(post => post.id === id);
 
     if (!post) {
+
         res.status(404);
 
         return res.json({
