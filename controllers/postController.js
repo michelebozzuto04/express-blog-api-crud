a87701f91f0a411eda1cddd7ffa31680bd5e5b1b
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-07-02 11:40:18 +0200

    feat: implement post creation functionality in store method

--- controllers/postController.js
+++ controllers/postController.js
@@ -30,7 +30,22 @@ function show(req, res) {
 }
 
 function store(req, res) {
-    res.send('New post created');
+    const newId = posts[posts.length - 1].id + 1;
+
+    const newPost = {
+        id: newId,
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image,
+        tags: req.body.tags,
+    }
+
+    posts.push(newPost);
+
+    console.log(posts);
+
+    res.status(201);
+    res.json(newPost);
 }
 
 function update(req, res) {
