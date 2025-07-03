8b47720182b36890c48571459cdc77662e991ef0
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-07-03 11:59:15 +0200

    feat: add error handling middleware for improved error management

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 const PostRouter = require('./routers/posts.js');
+const errorsHandler = require('./middlewares/errorsHandler.js');
+const notFound = require('./middlewares/notFound.js');
 
 app.use(express.json());
 
@@ -16,3 +18,6 @@ app.use('/posts', PostRouter);
 app.listen(PORT, () => {
     console.log(`Server live on http://localhost:${PORT}`);
 });
+
+app.use(errorsHandler);
+app.use(notFound);
